bd4962af55159ed87f4428920a7a1f25256cd956
Linus Westermeier <linus.westermeier@s-atiw.de>
2025-03-10 12:30:10 +0100

    Python Datei hinzugefügt

---
+++ gittest2.0.py
@@ -0,0 +1,9 @@
+###########################################################
+# Kopfkommentar
+# L. Westermeier
+# 10.03.2025
+# Dateiname: gittest2.0.py
+# Kommentar: GitHub testen
+##########################################################
+
+print("Hallo Git!")
